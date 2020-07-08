46e02dea736c605caa2473c5620eadaf7f17f41d
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-07-08 15:08:05 +0800

    chore：SHIZUKU 参数配置
    * SHIZUKU 与 Mirai HTTP API 通讯的参数设定

---
+++ configs/shizuku.go
@@ -0,0 +1,24 @@
+/*
+shizuku.go: SHIZUKU 配置参数
+Copyright (C) 2020-present  QianjuNakasumi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package configs
+
+const (
+	QQNumber     = 2103530879
+	MiraiAddress = "127.0.0.1:7958"
+	MiraiAuthKey = "LOVELIVENIJIGASAKISCHOOLIDOL&NIJIGASAKIMIRAINIJI-PRODUCT"
+)
