d2d49f8b3c954482af03b93f9c9e56477408b431
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-07-10 12:56:39 +0800

    feat：网络中间件、适用于Mirai对网络通讯进行封装模块化使用

---
+++ internal/uehara/networkMiddleware.go
@@ -0,0 +1,67 @@
+/*
+networkMiddleware.go: 适用于Mirai的网络中间件
+Copyright (C) 2020-present  QianjuNakasumi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package uehara
+
+import (
+	"io/ioutil"
+
+	"github.com/qianjunakasumi/shizuku/configs"
+	. "github.com/qianjunakasumi/shizuku/pkg/networkware"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+var (
+	json = jsoniter.ConfigCompatibleWithStandardLibrary
+)
+
+type Content map[string]interface{}
+
+func post(address string, content Content) (Content, error) {
+	bytes, err := json.Marshal(&content)
+	if err != nil {
+		return nil, err
+	}
+
+	req := new(Networkware)
+	req.Address = "http://" + configs.MiraiAddress + "/" + address
+	req.Body = bytes
+	req.Method = "POST"
+	req.Header = [][]string{
+		{"Content-Type", "application/json; charset=utf-8"},
+	}
+
+	res, err := req.Send()
+	if err != nil {
+		return nil, err
+	}
+	cont, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	content2 := make(Content)
+	if err = json.Unmarshal(cont, &content2); err != nil {
+		return nil, err
+	}
+	return content2, nil
+}
