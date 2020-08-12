f2d8710fc663cc6d61b0ec070278337405060c13
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-08-12 13:33:50 +0800

    史诗：自动插入当日计算数据

--- internal/shizuku/twitter/followersCount.go
+++ internal/shizuku/twitter/followersCount.go
@@ -17,13 +17,13 @@
 *
 *   func main(id string) (string, error) -- 获取 Twitter 对象粉丝数
 *
-*   type databaseFollowersTable struct     -- 数据库表字段结构
 *   type pushScheduleFollowersCount struct -- 保存 Twitter 粉丝数计算数据和提供相关方法的容器
 *     func (p *pushScheduleFollowersCount) getDatabaseData(id string) error -- 获取数据库保存的计算数据
 *     func (p *pushScheduleFollowersCount) calcTwitterFollowersData()       -- 计算输出数据
 *
-*   func fetchFollowersCount(calls map[string]string) (*message.Chain, error) -- 处理来自 Uehara 的调用
-*   func scheduleFollowersCount(name string) (*message.Chain, error)          -- 处理来自 定时任务函数 的调用
+*   func writeDB(a string, b string, c int32, d float32, e float32, f float32, g float32) -- 写入数据至数据库
+*   func fetchFollowersCount(calls map[string]string) (*message.Chain, error)             -- 处理来自 Uehara 的调用
+*   func scheduleFollowersCount(name string) (*message.Chain, error)                      -- 处理来自 定时任务函数 的调用
 *
 *----------------------------------------------------------------------------------------------------------------------*
 * Copyright:
@@ -57,22 +57,11 @@ import (
 
 	"github.com/qianjunakasumi/project-shizuku/configs"
 	"github.com/qianjunakasumi/project-shizuku/internal/uehara/message"
+	"github.com/qianjunakasumi/project-shizuku/internal/utils/database"
 
 	_ "github.com/go-sql-driver/mysql" // 连接数据库需要的包
 	"github.com/rs/zerolog/log"
 )
-
-type databaseFollowersTable struct {
-	id                uint32
-	date              string
-	account           uint8
-	followersCount    uint32
-	newFollowersCount uint32
-	huanbiRate        float32
-	ydayHuanbiRate    float32
-	dingjiRate        float32
-	syueDingjiRate    float32
-}
 
 type pushScheduleFollowersCount struct {
 	yesterdayFollowersCount uint32  // 昨日粉丝数
@@ -96,15 +85,18 @@ func main(id string) (string, error) {
 
 	res, err := get("graphql/-xfUfZsnR_zqjFd-IfrN5A/UserByScreenName?variables=%7B%22screen_name%22%3A%22" + id + "%22%2C%22withHighlightedLabel%22%3Atrue%7D")
 	if err != nil {
+
 		return "", err
+
 	}
 
 	count, ok := (((res["data"].(map[string]interface{}))["user"].(map[string]interface{}))["legacy"].(map[string]interface{}))["followers_count"].(float64)
 	if !ok {
 
 		log.Error().
-			Str("返回的内容是", fmt.Sprintf("%v", res)).
+			Str("内容", fmt.Sprintf("%v", res)).
 			Msg("解析错误")
+
 		return "", errors.New("错误")
 
 	}
@@ -113,28 +105,40 @@ func main(id string) (string, error) {
 
 }
 
-func fetchFollowersCount(calls map[string]string, info *message.MessageInfo) (*message.Chain, error) {
+func fetchFollowersCount(calls map[string]string, info *message.Info) (*message.Chain, error) {
 
 	var (
 		m          = new(message.Chain)
-		profile    = configs.Pipei(calls["account"], m, info.GroupName)
+		profile    = configs.AutoMatch(calls["account"], m, info.GroupName)
 		count, err = main(profile.Followers)
 	)
 
 	if err != nil {
+
 		return m, err
+
 	}
 
 	m.AddText("> " + profile.TwitterName + " 粉丝数：\n")
 
 	countUint, err := strconv.ParseUint(count, 10, 32)
-	wanCountUint := float64(countUint) / 10000
+	if err != nil {
 
-	// 输出千分位，阻止百分位非 9 数进一，删除最后一位小数，提高精确度
-	wanCountString := strconv.FormatFloat(wanCountUint, 'f', 3, 64)
-	wanCountString = wanCountString[:len(wanCountString)-1]
+		return m, err
 
-	m.AddText(count + " — 约 " + wanCountString + " 万")
+	}
+
+	var (
+		CountUint = float64(countUint) / 10000
+
+		// 目的：提高精确度
+		// 实现：输出千分位，阻止百分位非 9 数进一，删除最后一位小数
+		CountString = strconv.FormatFloat(CountUint, 'f', 3, 64)
+	)
+
+	CountString = CountString[:len(CountString)-1]
+
+	m.AddText(count + " — 约 " + CountString + " 万")
 
 	return m, nil
 
@@ -144,97 +148,115 @@ func (p *pushScheduleFollowersCount) getDatabaseData(id string) error {
 
 	db, err := sql.Open("mysql", configs.Conf.Databaseurl)
 	if err != nil {
+
 		return err
+
 	}
 
-	today := time.Now()
-	yesterday := today.AddDate(0, 0, -1).Format("20060102")
-	yuechu := today.AddDate(0, 0, 1-today.Day()).Format("20060102")
-	shangyue := today.AddDate(0, -1, 0).Format("20060102")
+	var (
+		t         = time.Now()
+		yesterday = t.AddDate(0, 0, -1).Format("20060102")
+		yuechu    = t.AddDate(0, 0, 1-t.Day()).Format("20060102")
+		shangyue  = t.AddDate(0, -1, 0).Format("20060102")
 
-	sql1 := fmt.Sprintf(`SELECT twitter_followers.*
-FROM twitter_followers
-WHERE twitter_followers.id LIKE '%v'
-ORDER BY twitter_followers.date DESC`, yesterday+id)
-	sql2 := fmt.Sprintf(`SELECT twitter_followers.*
-FROM twitter_followers
-WHERE twitter_followers.id LIKE '%v'
-ORDER BY twitter_followers.date DESC`, yuechu+id)
-	sql3 := fmt.Sprintf(`SELECT twitter_followers.*
-FROM twitter_followers
-WHERE twitter_followers.id LIKE '%v'
-ORDER BY twitter_followers.date DESC`, shangyue+id)
+		sql1 = fmt.Sprintf(`SELECT twitter_followers.* FROM twitter_followers WHERE twitter_followers.id LIKE '%v' ORDER BY twitter_followers.date DESC`, yesterday+id)
+		sql2 = fmt.Sprintf(`SELECT twitter_followers.* FROM twitter_followers WHERE twitter_followers.id LIKE '%v' ORDER BY twitter_followers.date DESC`, yuechu+id)
+		sql3 = fmt.Sprintf(`SELECT twitter_followers.* FROM twitter_followers WHERE twitter_followers.id LIKE '%v' ORDER BY twitter_followers.date DESC`, shangyue+id)
+	)
 
 	rows1, err := db.Query(sql1)
 	if err != nil {
+
 		return err
+
 	}
+
 	rows2, err := db.Query(sql2)
 	if err != nil {
+
 		return err
+
 	}
+
 	rows3, err := db.Query(sql3)
 	if err != nil {
+
 		return err
+
 	}
 
-	yesterdayData := new(databaseFollowersTable)
+	var (
+		yesterdayData = new(database.TwitterFollowers)
+		yuechuData    = new(database.TwitterFollowers)
+		shangyueData  = new(database.TwitterFollowers)
+	)
+
 	if rows1.Next() {
-		err = rows1.Scan(&yesterdayData.id,
-			&yesterdayData.date,
-			&yesterdayData.account,
-			&yesterdayData.followersCount,
-			&yesterdayData.newFollowersCount,
-			&yesterdayData.huanbiRate,
-			&yesterdayData.ydayHuanbiRate,
-			&yesterdayData.dingjiRate,
-			&yesterdayData.syueDingjiRate)
+
+		err = rows1.Scan(&yesterdayData.ID,
+			&yesterdayData.Date,
+			&yesterdayData.Account,
+			&yesterdayData.FollowersCount,
+			&yesterdayData.NewFollowersCount,
+			&yesterdayData.HuanbiRate,
+			&yesterdayData.YdayHuanbiRate,
+			&yesterdayData.DingjiRate,
+			&yesterdayData.SyueDingjiRate)
+
 		if err != nil {
+
 			return err
+
 		}
+
 	}
-	yuechuData := new(databaseFollowersTable)
+
 	if rows2.Next() {
-		err = rows2.Scan(&yuechuData.id,
-			&yuechuData.date,
-			&yuechuData.account,
-			&yuechuData.followersCount,
-			&yuechuData.newFollowersCount,
-			&yuechuData.huanbiRate,
-			&yuechuData.ydayHuanbiRate,
-			&yuechuData.dingjiRate,
-			&yuechuData.syueDingjiRate)
+
+		err = rows2.Scan(&yuechuData.ID,
+			&yuechuData.Date,
+			&yuechuData.Account,
+			&yuechuData.FollowersCount,
+			&yuechuData.NewFollowersCount,
+			&yuechuData.HuanbiRate,
+			&yuechuData.YdayHuanbiRate,
+			&yuechuData.DingjiRate,
+			&yuechuData.SyueDingjiRate)
+
 		if err != nil {
+
 			return err
+
 		}
+
 	}
-	shangyueData := new(databaseFollowersTable)
+
 	if rows3.Next() {
-		err = rows3.Scan(&shangyueData.id,
-			&shangyueData.date,
-			&shangyueData.account,
-			&shangyueData.followersCount,
-			&shangyueData.newFollowersCount,
-			&shangyueData.huanbiRate,
-			&shangyueData.ydayHuanbiRate,
-			&shangyueData.dingjiRate,
-			&shangyueData.syueDingjiRate)
+
+		err = rows3.Scan(&shangyueData.ID,
+			&shangyueData.Date,
+			&shangyueData.Account,
+			&shangyueData.FollowersCount,
+			&shangyueData.NewFollowersCount,
+			&shangyueData.HuanbiRate,
+			&shangyueData.YdayHuanbiRate,
+			&shangyueData.DingjiRate,
+			&shangyueData.SyueDingjiRate)
+
 		if err != nil {
+
 			return err
+
 		}
-	}
-
-	p.yesterdayFollowersCount = yesterdayData.followersCount // 昨日粉丝数
-	p.yuechuFollowersCount = yuechuData.followersCount       // 月初粉丝数
-	p.yesterdayHuanbiRate = yesterdayData.huanbiRate         // 昨日环比率
-	p.shangyueDingjiRate = shangyueData.dingjiRate           // 上月定基率
-
-	err = db.Close()
-	if err != nil {
-
-		return err
 
 	}
+
+	p.yesterdayFollowersCount = yesterdayData.FollowersCount // 昨日粉丝数
+	p.yuechuFollowersCount = yuechuData.FollowersCount       // 月初粉丝数
+	p.yesterdayHuanbiRate = yesterdayData.HuanbiRate         // 昨日环比率
+	p.shangyueDingjiRate = shangyueData.DingjiRate           // 上月定基率
+
+	defer db.Close()
 
 	return nil
 
@@ -253,9 +275,39 @@ func (p *pushScheduleFollowersCount) calcTwitterFollowersData() {
 		p.toYuechuDingji = (float32(p.todayFollowersCount)/float32(p.yuechuFollowersCount) - 1) * 1000 // 较本月月初定基
 
 	}
+
 	if p.shangyueDingjiRate != 0 {
 
 		p.toShangyueDingjiRate = p.toYuechuDingji - p.shangyueDingjiRate // 较上月定基率同比
+
+	}
+
+}
+
+func writeDB(a string, b string, c int32, d float32, e float32, f float32, g float32) {
+
+	var (
+		t    = time.Now()
+		id   = t.Format("20060102") + a
+		date = t.Format("2006-01-02")
+	)
+
+	insert, err := database.DB.Prepare(`INSERT INTO twitter_followers ( id, date, account, followersCount, newFollowersCount, huanbiRate, ydayHuanbiRate, dingjiRate, syueDingjiRate ) VALUES (?,	?, ?, ?, ?, ?, ?, ?, ?)`)
+	if err != nil {
+
+		log.Error().Err(err)
+
+		return
+
+	}
+	defer insert.Close()
+
+	_, err = insert.Exec(id, date, a, b, c, d, e, f, g)
+	if err != nil {
+
+		log.Error().Err(err)
+
+		return
 
 	}
 
@@ -292,7 +344,17 @@ func scheduleFollowersCount(name string) (*message.Chain, error) {
 	}
 
 	data.todayFollowersCount = uint32(follwersCount)
+
 	data.calcTwitterFollowersData()
+
+	go writeDB(profile.ID,
+		count,
+		data.newFollowersCount,
+		data.toYesterdayHuanbi,
+		data.toYesterdayHuanbiRate,
+		data.toYuechuDingji,
+		data.toShangyueDingjiRate,
+	)
 
 	m.AddText("> " + profile.TwitterName + " 粉丝数数据：\n")
 	m.AddText("早上好！数据日报订阅详情：\n")
