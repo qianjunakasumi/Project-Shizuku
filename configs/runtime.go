6013372234ad208b1b377d71c13b21a9f1a8da48
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-09-02 22:48:05 +0800

    更新：删除垃圾

--- configs/runtime.go
+++
@@ -1,7 +0,0 @@
-package configs
-
-import "github.com/qianjunakasumi/project-shizuku/internal/uehara/messagechain"
-
-var (
-	QuoteJob = make(map[uint32]*func(call string, info *messagechain.MessageInfo) (*messagechain.MessageChain, error))
-)
