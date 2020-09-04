edde3030c24a857959edb9e9a539890692f32f6e
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-09-04 21:45:51 +0800

    史诗：模糊获取 和 更全的信息

--- internal/shizuku/idol.go
+++ internal/shizuku/idol.go
@@ -32,20 +32,23 @@
 
 package shizuku
 
-import "math"
+import (
+	"math"
+	"strings"
+)
 
 type Idol struct {
-	ID       string   // 编号
-	Name     string   // 名
+	ID       string   // 主键
+	Name     string   // 声优名
 	PickName string   // 角色名
 	Key      []string // 关键字
-	Twitter  twitter
+	Twitter  twitter  // Twitter 相关
 }
 
 type twitter struct {
-	Followers string
-	Tweets    string
-	Push      func(x float64) float64
+	Followers string                  // 粉丝 API
+	Tweets    string                  // Tweet API
+	Push      func(x float64) float64 // 推送查询几率
 }
 
 var Idols = []Idol{{
@@ -63,7 +66,11 @@ var Idols = []Idol{{
 	"01",
 	"樱坂雫",
 	"前田 佳織里",
-	[]string{"樱坂", "雫", "前田", "佳織里", "佳织里", "kaor1n_n", "加智力"},
+	[]string{
+		"樱坂", "雫", "osaka", "shizuku",
+		"前田", "佳織里", "佳织里", "kaor1n_n", "加智力",
+		"_SHIZUKU默认检查专用",
+	},
 	twitter{
 		"kaor1n_n",
 		"880621944101404672",
@@ -74,7 +81,10 @@ var Idols = []Idol{{
 	"02",
 	"上原步梦",
 	"大西亜玖璃",
-	[]string{"上原", "步梦", "大西亜", "大西亚", "玖璃", "aguri_onishi", "阿兔嘭"},
+	[]string{
+		"上原", "步梦", "uehara", "ayumu",
+		"大西亜", "大西亚", "玖璃", "aguri_onishi", "阿兔嘭",
+	},
 	twitter{
 		"aguri_onishi",
 		"991283114885365761",
@@ -85,7 +95,10 @@ var Idols = []Idol{{
 	"03",
 	"中须霞",
 	"相良 茉優",
-	[]string{"相良", "茉優", "茉优", "MayuSgr", "麻油鸡"},
+	[]string{
+		"中须", "霞", "nakasu", "kasumi", "kasukasu", "ksks",
+		"相良", "茉優", "茉优", "MayuSgr", "麻油鸡",
+	},
 	twitter{
 		"mayusgr",
 		"1057615013282631680",
@@ -96,7 +109,10 @@ var Idols = []Idol{{
 	"04",
 	"朝香果林",
 	"久保田未夢",
-	[]string{"久保田", "未夢", "未梦", "iRis_k_miyu"},
+	[]string{
+		"朝香", "果林", "asaka", "karin",
+		"久保田", "未夢", "未梦", "iRis_k_miyu",
+	},
 	twitter{
 		"iris_k_miyu",
 		"2384783184",
@@ -107,7 +123,10 @@ var Idols = []Idol{{
 	"05",
 	"宫下爱",
 	"村上奈津実",
-	[]string{"村上", "奈津実", "奈津实", "natyaaaaaaan07"},
+	[]string{
+		"宫下", "爱", "miyashita", "ai",
+		"村上", "奈津実", "奈津实", "natyaaaaaaan07",
+	},
 	twitter{
 		"natyaaaaaaan07",
 		"760000974005997568",
@@ -118,7 +137,10 @@ var Idols = []Idol{{
 	"06",
 	"近江彼方",
 	"鬼頭明里",
-	[]string{"鬼頭", "鬼头", "明里", "kitoakari_1016"},
+	[]string{
+		"近江", "彼方", "konoe", "kanata",
+		"鬼頭", "鬼头", "明里", "kitoakari_1016",
+	},
 	twitter{
 		"kitoakari_1016",
 		"1141319903250534400",
@@ -129,7 +151,10 @@ var Idols = []Idol{{
 	"07",
 	"优木雪菜",
 	"楠木ともり",
-	[]string{"楠木", "ともり", "灯", "tomori_kusunoki"},
+	[]string{
+		"优木", "雪菜", "yuki", "setsuna",
+		"楠木", "ともり", "灯", "tomori_kusunoki",
+	},
 	twitter{
 		"tomori_kusunoki",
 		"847365153691582465",
@@ -140,7 +165,10 @@ var Idols = []Idol{{
 	"08",
 	"艾玛·维尔德",
 	"指出 毬亜",
-	[]string{"指出", "毬亜", "毬亚", "sashide_m"},
+	[]string{
+		"艾玛", "维尔德", "emma", "verde",
+		"指出", "毬亜", "毬亚", "sashide_m",
+	},
 	twitter{
 		"sashide_m",
 		"1075210326990217216",
@@ -151,7 +179,10 @@ var Idols = []Idol{{
 	"09",
 	"天王寺璃奈",
 	"田中ちえ美",
-	[]string{"田中", "ちえ美", "千惠美", "t_chiemi1006"},
+	[]string{
+		"天王寺", "璃奈", "tennoji", "rina",
+		"田中", "ちえ美", "千惠美", "t_chiemi1006",
+	},
 	twitter{
 		"t_chiemi1006",
 		"1176845285059747842",
@@ -162,7 +193,10 @@ var Idols = []Idol{{
 	"10",
 	"三船栞子",
 	"小泉萌香",
-	[]string{"三船", "栞子", "小泉", "萌香", "k_moeka_", "萌p"},
+	[]string{
+		"三船", "栞子", // TODO need more info...
+		"小泉", "萌香", "k_moeka_", "萌p",
+	},
 	twitter{
 		"k_moeka_",
 		"4110103573",
@@ -171,11 +205,22 @@ var Idols = []Idol{{
 		}},
 }}
 
-// TODO 干活 模糊匹配
-func XXXXX() {
+// FuzzyGetIdol 模糊匹配偶像
+func FuzzyGetIdol(n string) *Idol {
 
-	for _, _ = range Idols {
+	for _, v := range Idols {
+		for _, v2 := range v.Key {
+
+			p := strings.Index(n, v2)
+			p2 := strings.Index(v2, n)
+			if p != -1 || p2 != -1 {
+				return &v
+			}
+
+		}
 
 	}
+
+	return &Idols[1]
 
 }
