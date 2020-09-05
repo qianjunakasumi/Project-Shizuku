1e9f5e53e894982d0377dfed8adf0367e9b3c483
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-09-05 23:34:50 +0800

    其他：新增编译 Linux 用品

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,8 +2,8 @@ cd ../
 
 $root = "github.com/qianjunakasumi/project-shizuku/"
 $commitid = git rev-parse --short master
-$time = Get-Date -Format "yyMMddHH"
 
-go build -ldflags "-w -X ${root}configs.CommitId=${commitid} -X ${root}configs.BuildTime=${time}" -o shizuku.exe cmd/shizuku/main.go
+go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o shizuku.exe cmd/shizuku/main.go
 
-./upx.exe shizuku.exe
+$env:GOOS="linux"
+go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o shizuku cmd/shizuku/main.go
