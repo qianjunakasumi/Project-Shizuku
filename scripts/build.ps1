b156012ebb0c2889aa09acd44e74b49175e4d307
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-09-14 22:59:22 +0800

    其他：修改输出位置

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,7 +3,7 @@ cd ../
 $root = "github.com/qianjunakasumi/project-shizuku/"
 $commitid = git rev-parse --short master
 
-go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o shizuku.exe cmd/shizuku/main.go
+go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o build/shizuku.exe cmd/shizuku/main.go
 
 $env:GOOS="linux"
-go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o shizuku cmd/shizuku/main.go
+go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o build/shizuku cmd/shizuku/main.go
