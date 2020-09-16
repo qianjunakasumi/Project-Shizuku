ac846c01a38fe6e4e0264384d720ff52897ae4bd
QianjuNakasumi <qianjunakasumi@gmail.com>
2020-09-16 13:53:41 +0800

    其他：加入代码检查

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,7 +3,20 @@ cd ../
 $root = "github.com/qianjunakasumi/project-shizuku/"
 $commitid = git rev-parse --short master
 
+"开始检查代码错误..."
+
+go vet cmd/shizuku/main.go
+scripts/golangci-lint.exe run
+
+"请确认，按任意键开始编译"
+[Console]::ReadKey() | Out-Null
+
+"开始编译..."
+
 go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o build/shizuku.exe cmd/shizuku/main.go
 
 $env:GOOS="linux"
 go build -ldflags "-w -X ${root}configs.CommitId=${commitid}" -o build/shizuku cmd/shizuku/main.go
+
+"编译完成，按任意键退出..."
+[Console]::ReadKey() | Out-Null
